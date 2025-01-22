c1b8e80a3c2bf328fc291785d8d4c7c7ae08b83a
Omkar Prajapati <omkarprajapati3625@gmail.com>
2025-01-23 01:09:54 +0530

    delete api.js

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Routes
-router.use('/api/auth', require('../routes/auth'))
-router.use('/api/users', require('../routes/user'))
-
-module.exports = router
